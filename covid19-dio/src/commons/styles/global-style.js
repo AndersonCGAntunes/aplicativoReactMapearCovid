4835674fe845c9d9377f3821a7c94d92b345363c
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-05-25 18:35:06 -0300

    Configurações adicionais

--- covid19-dio/src/commons/styles/global-style.js
+++ covid19-dio/src/commons/styles/global-style.js
@@ -20,11 +20,25 @@ const globalStyle = createGlobalStyle`
     }
 
     #root {
-        background: URL(${CovidImg});
+        background: url(${CovidImg});
         height: 100%;
         width: 100%;
         background-size: cover;
         background-position: center center;
-    }`
+    }
+
+    .mb-2 {
+        margin-bottom: 16px;
+    }
+
+    .pt-2 {
+        padding-top: 16px;
+    }
+
+    .cursor {
+        cursor: pointer;
+    }
+    
+    `
 
     export default globalStyle
